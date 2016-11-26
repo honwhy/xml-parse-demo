9f15a176c5bd3811161a52bc5d014d2530b1f24a
honwhy.wang <honwhy19@163.com>
2016-11-26 16:42:40 +0800

    don't bother to deal with namespace

--- src/main/java/com/honey/util/XmlParseUtils.java
+++ src/main/java/com/honey/util/XmlParseUtils.java
@@ -1,15 +1,11 @@
 package com.honey.util;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.util.Iterator;
 import java.util.Map;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
@@ -33,22 +29,23 @@ import freemarker.template.Template;
 public class XmlParseUtils {
 	private DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 	private XPathFactory xpathFactory = XPathFactory.newInstance();
-	private String namespace;
-	private NamespaceContext namespaceContext;
 	
 	//freemarker
 	private Configuration config;
 	
-	public void init() {
-		this.namespaceContext = getNamespaceContext();
-	}
+	/**
+	 * 
+	 * @param param	数据内容
+	 * @param paths 表达式
+	 * @return
+	 * @throws Exception
+	 */
 	public Map<String,Object> parse(String param, Map<String,String> paths) throws Exception{
 		InputSource inputSource = new InputSource(new StringReader(param));
 		Document document = dbFactory.newDocumentBuilder().parse(inputSource);
 		Map<String,Object> map = Maps.newHashMap();
 		for(String key : paths.keySet()) {
 			XPath xpath = xpathFactory.newXPath();
-			xpath.setNamespaceContext(namespaceContext);
 			Node node = (Node) xpath.evaluate(paths.get(key), document, XPathConstants.NODE);
 			if(node == null) {
 				throw new Exception("node not found, xpath is " + paths.get(key));
@@ -57,7 +54,13 @@ public class XmlParseUtils {
 		}
 		return map;
 	}
-	
+	/**
+	 * 
+	 * @param param FreeMarker模板
+	 * @param context
+	 * @return
+	 * @throws Exception
+	 */
 	public String render(String param, Map<String,Object> context) throws Exception {
 		Configuration cfg = new Configuration();
 		StringTemplateLoader stringLoader = new StringTemplateLoader();
@@ -82,36 +85,6 @@ public class XmlParseUtils {
 		return engine.eval(expression);
 	}
 
-	private NamespaceContext getNamespaceContext() {
-		return new NamespaceContext() {
-
-			@Override
-			public String getNamespaceURI(String arg0) {
-				return namespace;
-			}
-
-			@Override
-			public String getPrefix(String arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public Iterator getPrefixes(String arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-		};
-	}
-	
-	public String getNamespace() {
-		return namespace;
-	}
-
-	public void setNamespace(String namespace) {
-		this.namespace = namespace;
-	}
 	
 	
 }
